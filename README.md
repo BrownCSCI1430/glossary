480346cf4c19907a6465a4edec20d271cccfec9e
Zachary Zhu <zach.z3394@gmail.com>
2022-01-20 16:52:41 -0500

    update mdToJson.py

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Glossary
 Working space for our glossary for 2022 Spring CSCI 1430 Computer Vision.
+
+# IMPORTANT
+
+When you update `COURSE_CONTENT.md`, you must run `python3 mdToJson.py COURSE_CONTENT.md` so that the updates are reflected in the webpage tooltips.
+
--- mdToJson.py
+++ mdToJson.py
@@ -1,15 +1,21 @@
 import json
 import sys
+import re
 
 glossary_map = {}
 
 curr_word = ''
 
+def slug(word):
+    return re.sub(r'\s', '-', re.sub(r'[\u2000-\u206F\u2E00-\u2E7F\'\\!"#$%&()*+,./:;<=>?@[\]^`{|}~]', '', re.sub(r'<[!\/a-z].*?>', '', word)))
+
 with open(sys.argv[1], 'r') as f:
     for line in f:
         stripped_line = line.strip()
+        if stripped_line.startswith('---'):
+            curr_word = ''
         if stripped_line.startswith('###'):
-            curr_word = stripped_line[4:]
+            curr_word = slug(stripped_line[4:]).lower()
             glossary_map[curr_word] = ''
         elif len(curr_word) > 0:
             # reset current word if new section is found
